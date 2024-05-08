f3ce30b32c21a59039e0d477d6cacd10d18e592a
Rodrigo Barreto <rodrigobarreto744@gmail.com>
2024-05-08 19:56:48 -0300

    chamando api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,21 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private httpClient: HttpClient) { }
+  private apiUrl = 'http://localhost:8080';
 
-  login(name: string, password:string){
-    return this.httpClient.post<LoginResponse>("/login", {name, password}).pipe(
-      tap((value) => {
-        sessionStorage.setItem("auth-token", value.token)
-        sessionStorage.setItem("username", value.name)
-      })
-    )
+  constructor(private httpClient: HttpClient) { } 
+
+  login(username: string, password: string): Observable<any> {
+    return this.httpClient.post<any>(`${this.apiUrl}/login`, { username, password });
   }
 }
